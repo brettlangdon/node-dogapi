brettlangdon/node-dogapi

commit 404e362717bfa6e7665ba94ad99314ea8f8aadcd

Let and Const all the things :hammer_and_wrench:

lib/api/comment.js
@@ -1,5 +1,5 @@
-var client = require("../client");
-var util = require("util");
+const client = require("../client");
+const util = require("util");
 
 /*section: comment
  *comment: create a new comment
@@ -29,7 +29,7 @@ function create(message, properties, callback){
         properties = {};
     }
 
-    var params = {
+    const params = {
         body: {
             message: message
         }
@@ -73,7 +73,7 @@ function update(commentId, message, handle, callback){
         handle = undefined;
     }
 
-    var params = {
+    const params = {
         body: {
             message: message
         }
@@ -134,8 +134,8 @@ module.exports = {
     },
     handleCli: function(subcommand, args, callback){
         if(subcommand === "create"){
-            var message = args._[4];
-            var properties = {};
+            const message = args._[4];
+            const properties = {};
             if(args["handle"]){
                 properties.handle = args["handle"];
             }
@@ -144,11 +144,11 @@ module.exports = {
             }
             create(message, properties, callback);
         } else if(subcommand === "update"){
-            var commentId = args._[4];
-            var message = args._[5];
+            const commentId = args._[4];
+            const message = args._[5];
             update(commentId, message, args["handle"], callback);
         } else if(subcommand === "remove"){
-            var commentId = args._[4];
+            const commentId = args._[4];
             remove(commentId, callback);
         } else {
             callback("unknown subcommand or arguments try `dogapi comment --help` for help", false);

lib/api/downtime.js
@@ -1,5 +1,5 @@
-var client = require("../client");
-var util = require("util");
+const client = require("../client");
+const util = require("util");
 
 /*section: downtime
  *comment: schedule a new downtime
@@ -30,7 +30,7 @@ function create(scope, properties, callback){
         properties = {};
     }
 
-    var params = {
+    const params = {
         body: {
             scope: scope
         }
@@ -81,7 +81,7 @@ function update(downtimeId, properties, callback){
         callback = properties;
         properties = {};
     }
-    var params = {
+    const params = {
         body: {}
     };
     if(typeof properties === "object"){
@@ -207,8 +207,8 @@ module.exports = {
         } else if(subcommand === "remove"){
             remove(args._[4], callback);
         } else if(subcommand === "create"){
-            var scope = args._[4];
-            var properties = {};
+            const scope = args._[4];
+            const properties = {};
             if(args["start"]){
                 properties.start = parseInt(args["start"]);
             }
@@ -220,8 +220,8 @@ module.exports = {
             }
             create(scope, properties, callback);
         } else if(subcommand === "update"){
-            var downtimeId = args._[4];
-            var properties = {};
+            const downtimeId = args._[4];
+            const properties = {};
             if(args["scope"]){
                 properties.scope = args["scope"];
             }

lib/api/embed.js
@@ -1,7 +1,7 @@
-var client = require("../client");
-var extend = require("extend");
-var json = require("../json");
-var querystring = require("querystring");
+const client = require("../client");
+const extend = require("extend");
+const json = require("../json");
+const querystring = require("querystring");
 
 /*section: embed
  *comment: create an embed graph of a metric query
@@ -15,14 +15,14 @@ var querystring = require("querystring");
  *  callback: function(err, res)
  *example: |
  *  ```javascript
- *  var dogapi = require("dogapi");
- *  var options = {
+ *  const dogapi = require("dogapi");
+ *  const options = {
  *    api_key: "api_key",
  *    app_key: "app_key"
  *  };
  *  dogapi.initialize(options);
- *  var query = "system.cpu.idle{*}";
- *  var graphJSON = {
+ *  const query = "system.cpu.idle{*}";
+ *  const graphJSON = {
  *         viz: "timeseries",
  *         requests: [
  *           {
@@ -33,7 +33,7 @@ var querystring = require("querystring");
  *           }
  *         ]
  *       }
- *  var options = {
+ *  const options = {
  *      timeframe: "1_hour",
  *      size: "xlarge",
  *      legend: "yes",
@@ -49,7 +49,7 @@ function create(graphJSON, options, callback){
         callback = options;
         options = {};
     }
-    var body = {
+    const body = {
         graph_json: JSON.stringify(graphJSON)
     };
     // Use `extend` to merge `options` into `body`
@@ -57,7 +57,7 @@ function create(graphJSON, options, callback){
     extend(body, options || {});
 
     // Create the request
-    var params = {
+    const params = {
         body: querystring.stringify(body),
         contentType: "application/x-www-form-urlencoded"
     };
@@ -72,7 +72,7 @@ function create(graphJSON, options, callback){
  *  callback: function(err, res)
  *example: |
  *  ```javascript
- *  var embedid = "foo";
+ *  const embedid = "foo";
  *  dogapi.embed.revoke(embedid, function(err, res){
  *    console.dir(res);
  *  });
@@ -105,7 +105,7 @@ function getAll(callback) {
  *  callback: function(err, res)
  *example: |
  *  ```javascript
- *  var embedId = "foo";
+ *  const embedId = "foo";
  *  dogapi.embed.get(embedId, function(err, res){
  *    console.dir(res);
  *  });
@@ -147,8 +147,8 @@ module.exports = {
     },
     handleCli: function(subcommand, args, callback) {
         if (args._.length > 4 && subcommand === "create") {
-            var graph_json = json.parse(args._[4]);
-            var options = {
+            const graph_json = json.parse(args._[4]);
+            const options = {
                 timeframe: args["timeframe"],
                 size: args["size"],
                 legend: args["legend"],
@@ -156,10 +156,10 @@ module.exports = {
             };
             create(graph_json, options, callback);
         } else if (args._.length > 4 && subcommand === "revoke") {
-            var embedId = args._[4];
+            const embedId = args._[4];
             revoke(embedId, callback);
         } else if (args._.length > 4 && subcommand === "get") {
-            var embedId = args._[4];
+            const embedId = args._[4];
             get(embedId, callback);
         } else if (subcommand === "getall") {
             getAll(callback);

lib/api/event.js
@@ -1,5 +1,5 @@
-var client = require("../client");
-var util = require("util");
+const client = require("../client");
+const util = require("util");
 
 /*section: event
  *comment: |
@@ -20,20 +20,20 @@ var util = require("util");
  *    function(err, res)
  *example: |
  *  ```javascript
- *   var dogapi = require("dogapi");
- *   var options = {
+ *   const dogapi = require("dogapi");
+ *   const options = {
  *     api_key: "api_key",
  *     app_key: "app_key"
  *   };
  *   dogapi.initialize(options);
- *   var title = "some new event";
- *   var text = "IT HAPPENED!";
+ *   const title = "some new event";
+ *   const text = "IT HAPPENED!";
  *   dogapi.event.create(title, text, function(err, res){
  *     console.dir(res);
  *   });
  *   title = "another event";
  *   text = "IT HAPPENED AGAIN!";
- *   var properties = {
+ *   const properties = {
  *     tags: ["some:tag"],
  *     alert_type: "error"
  *   };
@@ -54,7 +54,7 @@ function create(title, text, properties, callback){
     properties.title = title;
     properties.text = text;
 
-    var params = {
+    const params = {
         body: properties
     };
     client.request("POST", "/events", params, callback);
@@ -70,8 +70,8 @@ function create(title, text, properties, callback){
  *    function(err, res)
  *example: |
  *  ```javascript
- *   var dogapi = require("dogapi");
- *   var options = {
+ *   const dogapi = require("dogapi");
+ *   const options = {
  *     api_key: "api_key",
  *     app_key: "app_key"
  *   };
@@ -100,15 +100,15 @@ function get(eventId, callback){
  *    function(err, res)
  *example: |
  *  ```javascript
- *   var dogapi = require("dogapi");
- *   var options = {
+ *   const dogapi = require("dogapi");
+ *   const options = {
  *     api_key: "api_key",
  *     app_key: "app_key"
  *   };
  *   dogapi.initialize(options);
- *   var now = parseInt(new Date().getTime() / 1000);
- *   var then = now - 3600; // an hour ago
- *   var parameters = {
+ *   const now = parseInt(new Date().getTime() / 1000);
+ *   const then = now - 3600; // an hour ago
+ *   const parameters = {
  *     tags: "some:tag",
  *     sources: "jenkins"
  *   };
@@ -129,7 +129,7 @@ function query(start, end, parameters, callback){
     parameters.start = start;
     parameters.end = end;
 
-    var params = {
+    const params = {
         query: parameters
     };
 
@@ -169,9 +169,9 @@ module.exports = {
         if(subcommand === "get" && args._.length > 4){
             get(parseInt(args._[4]), callback);
         } else if(subcommand === "query" && args._.length > 5){
-            var from = parseInt(args._[4]);
-            var to = parseInt(args._[5]);
-            var parameters = {};
+            const from = parseInt(args._[4]);
+            const to = parseInt(args._[5]);
+            const parameters = {};
             if(args["sources"]){
                 parameters.sources = args["sources"];
             }
@@ -180,9 +180,9 @@ module.exports = {
             }
             query(from, to, parameters, callback);
         } else if(subcommand === "create" && args._.length > 5){
-            var title = args._[4];
-            var text = args._[5];
-            var properties = {};
+            const title = args._[4];
+            const text = args._[5];
+            const properties = {};
             if(args["priority"]){
                 properties.priority = args["priority"];
             }

lib/api/graph.js
@@ -1,5 +1,5 @@
-var client = require("../client");
-var embed = require("./embed");
+const client = require("../client");
+const embed = require("./embed");
 
 /*section: graph
  *comment: take a snapshot of a metric query
@@ -11,15 +11,15 @@ var embed = require("./embed");
  *  callback: function(err, res)
  *example: |
  *  ```javascript
- *  var dogapi = require("dogapi");
- *  var options = {
+ *  const dogapi = require("dogapi");
+ *  const options = {
  *    api_key: "api_key",
  *    app_key: "app_key"
  *  };
  *  dogapi.initialize(options);
- *  var query = "system.cpu.idle{*}";
- *  var to = dogapi.now();
- *  var from = to - 3600;  // an hour ago
+ *  const query = "system.cpu.idle{*}";
+ *  const to = dogapi.now();
+ *  const from = to - 3600;  // an hour ago
  *  dogapi.graph.snapshot(query, from, to, function(err, res){
  *    console.dir(res);
  *  });
@@ -30,7 +30,7 @@ function snapshot(query, from, to, eventQuery, callback){
         callback = eventQuery;
         eventQuery = undefined;
     }
-    var params = {
+    const params = {
         query: {
             metric_query: query,
             start: parseInt(from),
@@ -63,10 +63,10 @@ module.exports = {
     },
     handleCli: function(subcommand, args, callback){
         if (args._.length > 5 && subcommand === "snapshot"){
-            var query = args._[4];
-            var from = parseInt(args._[5]);
-            var to = parseInt(args._[6]);
-            var eventQuery = args["events"];
+            const query = args._[4];
+            const from = parseInt(args._[5]);
+            const to = parseInt(args._[6]);
+            const eventQuery = args["events"];
             snapshot(query, from, to, eventQuery, callback);
         } else {
             callback("unknown subcommand or arguments try `dogapi graph --help` for help", false);

commit 0dbab0c916e8ce11765039c00589439eead3ea97

Make linting green again :white_check_mark:
- disable some rules (put as warning)
- refactor some stuff
- fix some bugs introduced in 404e362 :bug:
- fix some original bugs (paramater, argument)

.eslintrc
@@ -20,20 +20,11 @@
   ],
   "rules": {
     "promise/no-native": "off",
-    "strict": "off"
+    "strict": "off",
+    "fp/no-arguments": "warn",
+    "no-param-reassign": "warn",
+    "prefer-rest-params": "warn",
+    "fp/no-loops": "warn"
   },
-  "overrides": [
-    {
-      "files": [ "scripts/migrate*.js", "lib/*.js" ],
-      "env": {
-        "mongo": true
-      }
-    },
-    {
-      "files": ["**/test/**"],
-      "rules": {
-        "unicorn/filename-case": "off"
-      }
-    }
-  ]
+  "overrides": []
 }

lib/api/comment.js
@@ -75,12 +75,10 @@ module.exports = function(client) {
 
     const params = {
       body: {
-        message
+        message,
+        handle: handle || undefined
       }
     };
-    if (handle) {
-      params.body.handle = properties.handle;
-    }
 
     client.request('PUT', util.format('/comments/%s', commentId), params, callback);
   }
@@ -150,7 +148,10 @@ module.exports = function(client) {
         const commentId = args._[4];
         remove(commentId, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi comment --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi comment --help` for help',
+          false
+        );
       }
     }
   };

lib/api/downtime.js
@@ -235,7 +235,10 @@ module.exports = function(client) {
         }
         update(downtimeId, properties, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi downtime --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi downtime --help` for help',
+          false
+        );
       }
     }
   };

lib/api/embed.js
@@ -162,7 +162,10 @@ module.exports = function(client) {
       } else if (subcommand === 'getall') {
         getAll(callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi embed --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi embed --help` for help',
+          false
+        );
       }
     }
   };

lib/api/event.js
@@ -118,7 +118,7 @@ module.exports = function(client) {
      *  ```
      */
   function query(start, end, parameters, callback) {
-    if (arguments.length < 4 && typeof argument[2] === 'function') {
+    if (arguments.length < 4 && typeof arguments[2] === 'function') {
       callback = parameters;
       parameters = {};
     }
@@ -203,7 +203,10 @@ module.exports = function(client) {
         }
         create(title, text, properties, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi event --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi event --help` for help',
+          false
+        );
       }
     }
   };

lib/api/graph.js
@@ -69,7 +69,10 @@ module.exports = function(client) {
         const eventQuery = args.events;
         snapshot(query, from, to, eventQuery, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi graph --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi graph --help` for help',
+          false
+        );
       }
     }
   };

lib/api/host.js
@@ -102,7 +102,7 @@ module.exports = function(client) {
         const hostname = args._[4];
         unmute(hostname, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi host --help` for help', false);
+        return callback('unknown subcommand or arguments try `dogapi host --help` for help', false);
       }
     }
   };

lib/api/index.js
@@ -10,7 +10,7 @@ module.exports = {
   monitor: require('./monitor'),
   screenboard: require('./screenboard'),
   search: require('./search'),
-  serviceCheck: require('./serviceCheck'),
+  serviceCheck: require('./service-check'),
   tag: require('./tag'),
   timeboard: require('./timeboard'),
   user: require('./user')

lib/api/metric.js
@@ -1,64 +1,6 @@
 module.exports = function(client) {
   /* section: metric
      *comment: |
-     *  submit a new metric
-     *params:
-     *  metric: the metric name
-     *  points: |
-     *    a single data point (e.g. `50`), an array of data points (e.g. `[50, 100]`)
-     *    or an array of `[timestamp, value]` elements (e.g. `[[now, 50], [now, 100]]`)
-     *  extra: |
-     *    optional, object which can contain the following keys
-     *    * host: the host source of the metric
-     *    * tags: array of "tag:value"'s to use for the metric
-     *    * metric_type|type: which metric type to use ("gauge" or "count") [default: gauge]
-     *  callback: |
-     *    function(err, res)
-     *example: |
-     *  ```javascript
-     *  const dogapi = require("dogapi");
-     *  const options = {
-     *    api_key: "api_key",
-     *    app_key: "app_key"
-     *  };
-     *  dogapi.initialize(options);
-     *  dogapi.metric.send("my.metric", 1000, function(err, results){
-     *    console.dir(results);
-     *  });
-     *  dogapi.metric.send("my.metric", [500, 1000], function(err, results){
-     *    console.dir(results);
-     *  });
-     *  const now = parseInt(new Date().getTime() / 1000);
-     *  dogapi.metric.send("my.metric", [[now, 1000]], function(err, results){
-     *    console.dir(results);
-     *  });
-     *  dogapi.metric.send("my.counter", 5, {type: "count"}, function(err, results){
-     *    console.dir(results);
-     *  });
-     *  ```
-     */
-  function send(metric, points, extra, callback) {
-    if (arguments.length < 4 && typeof arguments[2] === 'function') {
-      callback = extra;
-      extra = {};
-    }
-    extra = extra || {};
-    const series = [
-      {
-        metric,
-        points,
-        host: extra.host,
-        tags: extra.tags,
-        // DEV: For backwards compatibility, allow `metric_type`
-        type: extra.type || extra.metric_type
-      }
-    ];
-
-    send_all(series, callback);
-  }
-
-  /* section: metric
-     *comment: |
      *  send a list of metrics
      *params:
      *  metrics: |
@@ -117,7 +59,6 @@ module.exports = function(client) {
         //   500 => [<timestamp>, 500]
         //   [<timestamp>, 500] => unchanged
         if (!Array.isArray(point)) {
-          const now = parseInt(new Date().getTime() / 1000);
           point = [now, point];
         }
         return point;
@@ -142,11 +83,69 @@ module.exports = function(client) {
 
   /* section: metric
      *comment: |
+     *  submit a new metric
+     *params:
+     *  metric: the metric name
+     *  points: |
+     *    a single data point (e.g. `50`), an array of data points (e.g. `[50, 100]`)
+     *    or an array of `[timestamp, value]` elements (e.g. `[[now, 50], [now, 100]]`)
+     *  extra: |
+     *    optional, object which can contain the following keys
+     *    * host: the host source of the metric
+     *    * tags: array of "tag:value"'s to use for the metric
+     *    * metric_type|type: which metric type to use ("gauge" or "count") [default: gauge]
+     *  callback: |
+     *    function(err, res)
+     *example: |
+     *  ```javascript
+     *  const dogapi = require("dogapi");
+     *  const options = {
+     *    api_key: "api_key",
+     *    app_key: "app_key"
+     *  };
+     *  dogapi.initialize(options);
+     *  dogapi.metric.send("my.metric", 1000, function(err, results){
+     *    console.dir(results);
+     *  });
+     *  dogapi.metric.send("my.metric", [500, 1000], function(err, results){
+     *    console.dir(results);
+     *  });
+     *  const now = parseInt(new Date().getTime() / 1000);
+     *  dogapi.metric.send("my.metric", [[now, 1000]], function(err, results){
+     *    console.dir(results);
+     *  });
+     *  dogapi.metric.send("my.counter", 5, {type: "count"}, function(err, results){
+     *    console.dir(results);
+     *  });
+     *  ```
+     */
+  function send(metric, points, extra, callback) {
+    if (arguments.length < 4 && typeof arguments[2] === 'function') {
+      callback = extra;
+      extra = {};
+    }
+    extra = extra || {};
+    const series = [
+      {
+        metric,
+        points,
+        host: extra.host,
+        tags: extra.tags,
+        // DEV: For backwards compatibility, allow `metric_type`
+        type: extra.type || extra.metric_type
+      }
+    ];
+
+    send_all(series, callback);
+  }
+
+  /* section: metric
+     *comment: |
      *  make a metric query
      *params:
      *  from: POSIX timestamp for start of query
      *  to: POSIX timestamp for end of query
-     *  query: the string query to perform (e.g. "system.cpu.idle{*}by{host}")
+     *  q: the string query to perform (e.g. "system.cpu.idle{*}by{host}")
      *  callback: function(err, res)
      *example: |
      *  ```javascript
@@ -164,12 +163,12 @@ module.exports = function(client) {
      *  });
      *  ```
      */
-  function query(from, to, query, callback) {
+  function query(from, to, q, callback) {
     const params = {
       query: {
         from,
         to,
-        query
+        query: q
       }
     };
     client.request('GET', '/query', params, callback);
@@ -213,7 +212,10 @@ module.exports = function(client) {
         const q = args._[6];
         query(from, to, q, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi metric --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi metric --help` for help',
+          false
+        );
       }
     }
   };

lib/api/monitor.js
@@ -379,15 +379,8 @@ module.exports = function(client) {
       ];
     },
     handleCli(subcommand, args, callback) {
-      const states = [];
-      if (args.states) {
-        states = args.states.split(',');
-      }
-
-      const tags = [];
-      if (args.tags) {
-        tags = args.tags.split(',');
-      }
+      const states = args.states ? args.states.split(',') : [];
+      const tags = args.tags ? args.tags.split(',') : [];
 
       const name = args.name;
       const message = args.message;
@@ -397,10 +390,10 @@ module.exports = function(client) {
         get(monitorId, states, callback);
       } else if (subcommand === 'getall') {
         const options = {};
-        if (states.length) {
+        if (states.length > 0) {
           options.group_states = states;
         }
-        if (tags.length) {
+        if (tags.length > 0) {
           options.tags = tags;
         }
         getAll(options, callback);
@@ -448,7 +441,10 @@ module.exports = function(client) {
         }
         update(monitorId, query, properties, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi monitor --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi monitor --help` for help',
+          false
+        );
       }
     }
   };

lib/api/screenboard.js
@@ -343,7 +343,10 @@ module.exports = function(client) {
 
         create(boardTitle, widgets, options, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi screenboard --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi screenboard --help` for help',
+          false
+        );
       }
     }
   };

lib/api/search.js
@@ -19,10 +19,10 @@ module.exports = function(client) {
      *  });
      *  ```
      */
-  function query(query, callback) {
+  function query(q, callback) {
     const params = {
       query: {
-        q: query
+        q
       }
     };
     client.request('GET', '/search', params, callback);
@@ -44,7 +44,10 @@ module.exports = function(client) {
       if (subcommand === 'query' && args._.length > 4) {
         query(args._[4], callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi search --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi search --help` for help',
+          false
+        );
       }
     }
   };

lib/api/service-check.js
@@ -28,7 +28,7 @@ module.exports = function(client) {
      *   });
      *  ```
      */
-  function check(check, hostName, status, parameters, callback) {
+  function check(checkName, hostName, status, parameters, callback) {
     if (arguments.length < 5 && typeof arguments[3] === 'function') {
       callback = parameters;
       parameters = {};
@@ -38,8 +38,9 @@ module.exports = function(client) {
       parameters = {};
     }
 
-    parameters.check = check;
-    (parameters.host_name = hostName), (parameters.status = status);
+    parameters.check = checkName;
+    parameters.host_name = hostName;
+    parameters.status = status;
 
     const params = {
       body: parameters
@@ -73,14 +74,14 @@ module.exports = function(client) {
           parameters.time = parseInt(args.time);
         }
         if (args.message) {
-          paramaters.message = args.message;
+          parameters.message = args.message;
         }
         if (args.tags) {
           parameters.tags = args.tags.split(',');
         }
         check(args._[4], args._[5], parseInt(args._[6]), parameters, callback);
       } else {
-        callback('not enough arguments try `dogapi servicecheck --help` for help', false);
+        return callback('not enough arguments try `dogapi servicecheck --help` for help', false);
       }
     }
   };

lib/api/tag.js
@@ -1,5 +1,3 @@
-const util = require('util');
-
 module.exports = function(client) {
   /* section: tag
      *comment: |
@@ -254,7 +252,7 @@ module.exports = function(client) {
       } else if (subcommand === 'delete') {
         remove(host, source, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi tag --help` for help', false);
+        return callback('unknown subcommand or arguments try `dogapi tag --help` for help', false);
       }
     }
   };

lib/api/timeboard.js
@@ -67,7 +67,7 @@ module.exports = function(client) {
         graphs
       }
     };
-    if (Array.isArray(templateVariables) && templateVariables.length) {
+    if (Array.isArray(templateVariables) && templateVariables.length > 0) {
       params.body.template_variables = templateVariables;
     }
 
@@ -140,7 +140,7 @@ module.exports = function(client) {
         graphs
       }
     };
-    if (Array.isArray(templateVariables) && templateVariables.length) {
+    if (Array.isArray(templateVariables) && templateVariables.length > 0) {
       params.body.template_variables = templateVariables;
     }
 
@@ -251,10 +251,7 @@ module.exports = function(client) {
         const title = args._[4];
         const description = args._[5];
         const graphs = json.parse(args._[6]);
-        const templateVariables = [];
-        if (args.tmpvars) {
-          templateVariables = json.parse(args.tmpvars);
-        }
+        const templateVariables = args.tmpvars ? json.parse(args.tmpvars) : [];
 
         create(title, description, graphs, templateVariables, callback);
       } else if (subcommand === 'update') {
@@ -262,14 +259,14 @@ module.exports = function(client) {
         const title = args._[5];
         const description = args._[6];
         const graphs = json.parse(args._[7]);
-        const templateVariables = [];
-        if (args.tmpvars) {
-          templateVariables = json.parse(args.tmpvars);
-        }
+        const templateVariables = args.tmpvars ? json.parse(args.tmpvars) : [];
 
         update(dashId, title, description, graphs, templateVariables, callback);
       } else {
-        callback('unknown subcommand or arguments try `dogapi timeboard --help` for help', false);
+        return callback(
+          'unknown subcommand or arguments try `dogapi timeboard --help` for help',
+          false
+        );
       }
     }
   };

lib/client.js
@@ -2,7 +2,7 @@ const https = require('https');
 const url = require('url');
 const util = require('util');
 const extend = require('extend');
-const _ = require('lodash');
+const _ = require('lodash/fp');
 const json = require('./json');
 
 /* section: client
@@ -67,15 +67,12 @@ DatadogMetricClient.prototype.request = function(method, path, params, callback)
     query
   });
 
-  const http_options = _.assign(
-    {
-      hostname: this.api_host,
-      port: 443,
-      method: method.toUpperCase(),
-      path
-    },
-    this.http_options
-  );
+  const http_options = _.assign(this.http_options, {
+    hostname: this.api_host,
+    port: 443,
+    method: method.toUpperCase(),
+    path
+  });
 
   if (this.proxy_agent) {
     http_options.agent = this.proxy_agent;

lib/index.js
@@ -1,6 +1,8 @@
-const _ = require('lodash');
+const _ = require('lodash/fp');
 const api = require('./api');
 const Client = require('./client');
+
+const forEach = _.forEach.convert({cap: false});
 
 const initialClient = new Client({});
 /* section: dogapi
@@ -50,15 +52,15 @@ function initialize(options) {
 
 function DogApi(options) {
   const client = new Client(options || {});
-  _.forEach(api, (value, key) => {
+  forEach((value, key) => {
     this[key] = value(client);
-  });
+  }, api);
 }
 
 DogApi.initialize = initialize;
-_.forEach(api, (value, key) => {
+forEach((value, key) => {
   DogApi[key] = value(initialClient);
-});
+}, api);
 
 /* section: dogapi
  *comment: get the current POSIX timestamp